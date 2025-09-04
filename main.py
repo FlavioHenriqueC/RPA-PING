ce679a7a8d327ef1d70266e40705493a75718f89
FlavioHenriqueC <flaviohenrique2355@gmail.com>
2025-09-03 23:08:26 -0300

    adicionando função de incrementar resultados em arquivo csv

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import subprocess
 import csv
 
 
-# Lendo o CSV
+# comando que lê o arquivo
 df = pd.read_csv("maquinas.csv", sep=";")
-
+resultados = []
 # Supondo que exista uma coluna chamada "nome"
 for nome in df["Nome"]:
     comando = f'ping {nome}'  # exemplo de comando
-    subprocess.run(comando, shell=True)
+    resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    resultados.append([nome])
+
+    df_resultados = pd.DataFrame(resultados, columns=["Maquina"])
+
+    df_resultados.to_csv("resultado.csv", index=False, encoding="utf-8-sig")
